cb01a2c937fd63a3c3bc0efa02a10625b62d0dde
Chris Jaynes <chris.jaynes@classdojo.com>
2022-04-28 16:14:22 -0500

    Add clarifying comment to setupProxy

--- packages/client/src/setupProxy.js
+++ packages/client/src/setupProxy.js
@@ -1,3 +1,8 @@
+// A rather magical feature of react-scripts allows us to
+// specify dev-time proxy settings in this file.
+// It seems this is necessary to allow GraphQL's WebSocket
+// link through the proxy.
+
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function (app) {
