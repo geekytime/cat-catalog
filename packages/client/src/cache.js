73470e7b735f34173ec357cf55f66a80b9ab64fb
Chris Jaynes <chris.jaynes@classdojo.com>
2022-04-28 16:10:30 -0500

    Resolve a warning related to caching and deletes

--- packages/client/src/cache.js
+++ packages/client/src/cache.js
@@ -1,6 +1,22 @@
 import { InMemoryCache } from '@apollo/client'
 
 export const createCache = () => {
-  const cache = new InMemoryCache()
+  const cache = new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          cats: {
+            merge (existing = [], incoming = []) {
+              // Apollo client was throwing warnings when we removed
+              // cats via subscription updates.
+              // This seems to resolve the warnings, but does it have
+              // other impacts?
+              return incoming
+            }
+          }
+        }
+      }
+    }
+  })
   return cache
 }
